93ab8c73c9a6ec92ec30223f848d22ccda841fd1
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2022-02-12 01:15:38 -0500

    fixed stacking issue on resize

--- src/Reel.js
+++ src/Reel.js
@@ -5,14 +5,14 @@ function Reel() {
     <div>
       <h1 className="text-center">Christina Chin</h1>
       <h2 className="text-center">Reel</h2>
-      <div className="d-flex justify-content-around">
-        <div>
-          <iframe className="mr-5" src="https://www.youtube.com/embed/EU0LjkAUQ6E"></iframe>
-        </div>
-        <div>
-          <p className="font-weight-bold">SPIDER-MAN: Across The Spider-verse Trailer (2022) </p>
-          <p>Into The Spider-verse 2 is a film where Christina was the lead cinematographer. She was on camera on almost all the shots and worked directly with the directors</p>
-        </div>
+      <div className="row d-flex justify-cont">
+          <div className="col-md-6 embed-responsive embed-responsive-16by9">
+            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/EU0LjkAUQ6E"></iframe>
+          </div>
+          <div className="col-md-4">
+            <p className="font-weight-bold">SPIDER-MAN: Across The Spider-verse Trailer (2022) </p>
+            <p>Into The Spider-verse 2 is a film where Christina was the lead cinematographer. She was on camera on almost all the shots and worked directly with the directors</p>
+          </div>
       </div>
     </div>
   );
